2bd52bf3f372336c601afaadf8cc36d4c4843b4f
Salvatore949 <salvo.mess49@gmail.com>
2021-11-08 01:03:32 +0100

    feed stampati

--- js/script.js
+++ js/script.js
@@ -1,55 +1,111 @@
-// Creo l'arrey 
-//Inserisco le informazioni necessarie:(nome autore,foto profilo,data,testo del post,immagine,numero di likes)
-//Stampo attraverso InnnerHtml tutti i feed
-// Creiamo il nostro array di oggetti che rappresentano ciascun post.array
-// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: }
+// Creo l'array di oggetti che rappresentano ciascun post
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:{
 // nome autore,
 // foto profilo,
 // data,
 // testo del post,
-// immagine (non tutti i post devono avere una immagine) [quindi gestisco il caso],
+// immagine (non tutti i post devono avere una immagine,
 // numero di likes.
+// stabilisco che se i=2 la mainimage non verrà mostrata
 // - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+
 // - Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
-
-
-
 // prendo i riferimenti di tutti i pulsanti Mi Piace in pagina (mi ritorna un array)
 // per ogni elemento nell'array => aggiungo listener sul click del pulsante 
 // nel listener incremento di 1 il numero di like del post
 
-const post = [
-      {
-          "autore" : "autore",
-          "foto": "https://media.istockphoto.com/photos/happy-multiethnic-peoples-headshot-picture-id493763323?b=1&k=20&m=493763323&s=170667a&w=0&h=TT77zxSkv5Vt8rVKzzbwQci3dnibRNIV_eGVIG-dbBw=",
-          "data":"3/10",
-          "testo":"lorem ipsum dolor sit amet",
-          "likes":""
+var cards = [
+
+    {   
+        "photo": "https://unsplash.it/300/300?image=15",
+        "author":"John",
+        "date":"6 mesi fa",
+        "text": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "main_photo":"https://unsplash.it/600/300?image=171",
+        "photo_alt":"drops",
+        "likes":"20"
     },
 
-    {
-        "autore" : "autore",
-        "foto": "https://media.istockphoto.com/photos/happy-multiethnic-peoples-headshot-picture-id493763323?b=1&k=20&m=493763323&s=170667a&w=0&h=TT77zxSkv5Vt8rVKzzbwQci3dnibRNIV_eGVIG-dbBw=",
-        "data":"3/10",
-        "testo":"lorem ipsum dolor sit amet",
-        "likes":""
+    {   
+        "photo": "https://unsplash.it/300/300?image=15",
+        "author":"philip",
+        "date":"8 mesi fa",
+        "text": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "main_photo":"https://unsplash.it/600/300?image=171",
+        "photo_alt":"drops",
+        "likes":"30"
     },
 
-    {
-        "autore" : "autore",
-        "foto": "https://media.istockphoto.com/photos/happy-multiethnic-peoples-headshot-picture-id493763323?b=1&k=20&m=493763323&s=170667a&w=0&h=TT77zxSkv5Vt8rVKzzbwQci3dnibRNIV_eGVIG-dbBw=",
-        "data":"3/10",
-        "testo":"lorem ipsum dolor sit amet",
-        "likes":""
+    {   
+        "photo": "https://unsplash.it/300/300?image=15",
+        "author":"Ilary",
+        "date":"11 mesi fa",
+        "text": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "main_photo":"https://unsplash.it/600/300?image=171",
+        "photo_alt":"drops",
+        "likes":"15"
     },
 
-    {
-        "autore" : "autore",
-        "foto": "https://media.istockphoto.com/photos/happy-multiethnic-peoples-headshot-picture-id493763323?b=1&k=20&m=493763323&s=170667a&w=0&h=TT77zxSkv5Vt8rVKzzbwQci3dnibRNIV_eGVIG-dbBw=",
-        "data":"3/10",
-        "testo":"lorem ipsum dolor sit amet",
-        "likes":""
+    {   
+        "photo": "https://unsplash.it/300/300?image=15",
+        "author":"Ilary",
+        "date":"11 mesi fa",
+        "text": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "main_photo":"https://unsplash.it/600/300?image=171",
+        "photo_alt":"drops",
+        "likes":"15"
     }
 
-]
+];
+
+    console.log(cards)
+
+    for(let i = 0; i < cards.length; i++) {
+        let image = cards[i].photo;
+        let words = cards[i].author;
+        let ago = cards[i].date;
+        let lorem = cards[i].text;
+        let rappresentation = cards[i].main_photo;
+        let want = cards[i].likes;
+        let etitle = cards[i].photo_alt; 
+
+        if ( i === 2){
+            rappresentation = "";
+            etitle = "";
+        }
+
+        document.querySelector('.post').innerHTML += `
+        <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${image}" alt="${words}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${words}</div>
+                        <div class="post-meta__time">${ago}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${lorem}</div>
+            <div class="post__image">
+                <img src="${rappresentation}" alt="${etitle}">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${want}</b> persone
+                    </div>
+                </div> 
+            </div>
+        </div>`        
+    }
+
+
+
 
